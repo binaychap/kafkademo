85747e43bba6b513668ceb4798a3d5d8d018d617
binai-rai <binai.rai@mheducation.com>
2019-02-06 22:29:11 -0500

    kafka producer and consumer test

---
+++ src/main/java/com/binay/kafkaDemo/KafkaConsumerApp.java
@@ -0,0 +1,50 @@
+package com.binay.kafkaDemo;
+
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.ArrayList;
+import java.util.Properties;
+
+public class KafkaConsumerApp {
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9091, localhost:9092");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer"); // type contract extends to consumer
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer"); // type contract extends to consumer
+        props.put("group.id", "test-consumer-group");
+        KafkaConsumer<String, String> myConsumer = new KafkaConsumer<>(props);
+
+        ArrayList<String> topics = new ArrayList<String>();
+        topics.add("deploy");
+
+        myConsumer.subscribe(topics);
+        try {
+            while (true) {
+                ConsumerRecords<String, String> records = myConsumer.poll(30L);
+                for (ConsumerRecord<String, String> record : records) {
+                    System.out.println(
+                            String.format("Topic: %s, " +
+                                            "Partition: %d, " +
+                                            "Offset: %d, " +
+                                            "Key: %s, " +
+                                            "Value: %s",
+                                    record.topic(),
+                                    record.partition(),
+                                    record.offset(),
+                                    record.key(),
+                                    record.value())
+                    );
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            myConsumer.close();
+        }
+    }
+
+}
---
+++ src/main/java/com/binay/kafkaDemo/KafkaProducerApp.java
@@ -0,0 +1,26 @@
+package com.binay.kafkaDemo;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class KafkaProducerApp {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9091, localhost:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        KafkaProducer<String, String> myProducer = new KafkaProducer<>(props);
+        try {
+            for (int i = 0; i < 150000000; i++) {
+                myProducer.send(new ProducerRecord<>("deploy", Integer.toString(i), MessageUtils.jsonMsg + Integer.toString(i)));
+                System.out.println("message sent "+ i);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            myProducer.close();
+        }
+    }
+}

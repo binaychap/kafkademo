9d0da2e4ebe6dc01697065210acf340575cec926
binai-rai <binai.rai@mheducation.com>
2019-02-07 15:16:32 -0500

    test

--- src/main/java/com/binay/kafkaDemo/MessageUtils.java
+++ src/main/java/com/binay/kafkaDemo/MessageUtils.java
@@ -9,23 +9,6 @@ public class MessageUtils {
             "        \"name\": \"main_window\",\n" +
             "        \"width\": 500,\n" +
             "        \"height\": 500\n" +
-            "    },\n" +
-            "    \"image\": { \n" +
-            "        \"src\": \"Images/Sun.png\",\n" +
-            "        \"name\": \"sun1\",\n" +
-            "        \"hOffset\": 250,\n" +
-            "        \"vOffset\": 250,\n" +
-            "        \"alignment\": \"center\"\n" +
-            "    },\n" +
-            "    \"text\": {\n" +
-            "        \"data\": \"Click Here\",\n" +
-            "        \"size\": 36,\n" +
-            "        \"style\": \"bold\",\n" +
-            "        \"name\": \"text1\",\n" +
-            "        \"hOffset\": 250,\n" +
-            "        \"vOffset\": 100,\n" +
-            "        \"alignment\": \"center\",\n" +
-            "        \"onMouseUp\": \"sun1.opacity = (sun1.opacity / 100) * 90;\"\n" +
             "    }\n" +
             "}} ";
 }
